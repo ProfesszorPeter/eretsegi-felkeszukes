97d5fb93aff3316e8bfaa1292d2234b38abcd622
Simon Péter <simonpetymeg@gmail.com>
2023-01-31 21:53:42 +0100

    hányados javítása

--- 4.py
+++ 4.py
@@ -4,12 +4,18 @@ num2= int(input("Add meg a második számot: "))
 osszeg = num2 + num1
 kulonbseg = num1 - num2
 szorzat = num2 * num1
-hanyados = num1 / num2
+#hanyados = num1 / num2
 
 print(osszeg)
 print(kulonbseg)
 print(szorzat)
-print(hanyados)
+#print(hanyados)
+
+#hanyados
+if num1 % num2 == 0:
+    print(num1 / num2)
+else:
+    exit 
 
 #def hanyados:
 #    if num1 % num2 == 0:
