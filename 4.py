ba011c0c916754fb6d2a65d587b2eda56df64dff
Simon Péter <simonpetymeg@gmail.com>
2023-01-31 21:12:33 +0100

    4. feladat

---
+++ 4.py
@@ -0,0 +1,16 @@
+num1= int(input("Add meg az első számot:"))
+num2= int(input("Add meg a második számot: "))
+
+osszeg = num2 + num1
+kulonbseg = num1 - num2
+szorzat = num2 * num1
+hanyados = num1 / num2
+
+print(osszeg)
+print(kulonbseg)
+print(szorzat)
+print(hanyados)
+
+#def hanyados:
+#    if num1 % num2 == 0:
+#        print(hanyados)
