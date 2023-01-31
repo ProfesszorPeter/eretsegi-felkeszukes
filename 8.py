4f19c6edf728a30dc44c0ef1814e13f4daa780a1
Simon Péter <simonpetymeg@gmail.com>
2023-01-31 22:40:48 +0100

    számtani és mértani közép

---
+++ 8.py
@@ -0,0 +1,10 @@
+import math
+
+a = int(input("Adja meg az első számot: "))
+b = int(input("Adja meg a második számot: "))
+
+atlag = (a + b) / 2 
+mertani_kozep = math.sqrt(a*b)
+
+print(atlag)
+print(mertani_kozep)
